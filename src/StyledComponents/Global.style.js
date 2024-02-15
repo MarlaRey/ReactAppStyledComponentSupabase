7b8919899110cb641a0b8af9765b86b7abd78bae
MarlaRey <hejmarlenehej@gmail.com>
2024-02-15 10:11:44 +0100

    kontakt til GlobalStyle.js

---
+++ src/StyledComponents/Global.style.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+body{
+    background-color: #000;
+
+}
+h1{
+    font-size: 2rem;
+}
+h2{
+    fontsize: 1.5rem;
+}
+
+`
